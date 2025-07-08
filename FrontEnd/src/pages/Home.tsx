dcb764fcdcd7da1a6d2a33cd5c7b9a0dd4f1ff62
Ihaxxxx <mubashir.mandvia@gmail.com>
2025-07-08 15:41:20 +0500

    Started Graphs

--- FrontEnd/src/pages/Home.tsx
+++ FrontEnd/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import {ChartBarLabelCustom}  from '../components/BarChart.tsx'
 
 function Home() {
   return (
     <>
     <Navbar></Navbar>
     <div>Home</div>
+    <ChartBarLabelCustom></ChartBarLabelCustom>
     </>
   )
 }
